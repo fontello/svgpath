7d7f3ae593da1d10504aad6520add4df6564582d
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-04-13 10:01:49 +0300

    Tests cleanup & path parser coverage improve

--- test/path_parse.js
+++ test/path_parse.js
@@ -7,7 +7,7 @@ var assert  = require('assert');
 var fs      = require('fs');
 var path    = require('path');
 
-var SvgPath = require('../');
+var svgpath = require('../');
 
 
 describe('Path parse', function () {
@@ -17,27 +17,44 @@ describe('Path parse', function () {
 
     for (var i = 0; i < batch.length; i++) {
       if (!batch[i]) { continue; }
-      assert.equal(batch[i], new SvgPath(batch[i]).toString());
+      assert.equal(batch[i], svgpath(batch[i]).toString());
     }
   });
 
 
   it('empty string', function () {
-    assert.equal('', new SvgPath('').toString());
+    assert.equal(svgpath('').toString(), '');
   });
 
 
   it('line terminators', function () {
-    assert.equal('M0 0l2-3z', new SvgPath('M0\r 0\nl2-3\nz').toString());
+    assert.equal(svgpath('M0\r 0\n\u1680l2-3\nz').toString(), 'M0 0l2-3z');
   });
 
 
   it('params formats', function () {
-    assert.equal('M0 0', new SvgPath('M 0.0 0.0').toString());
-    assert.equal('M100 0', new SvgPath('M 1e+2 0').toString());
-    assert.equal('M100 0', new SvgPath('M +1e+2 0').toString());
-    assert.equal('M0.01 0', new SvgPath('M 1e-2 0').toString());
-    assert.equal('M0.001 0', new SvgPath('M 0.1e-2 0').toString());
-    assert.equal('M0.001 0', new SvgPath('M .1e-2 0').toString());
+    assert.equal(svgpath('M 0.0 0.0').toString(),  'M0 0');
+    assert.equal(svgpath('M 1e2 0').toString(),    'M100 0');
+    assert.equal(svgpath('M 1e+2 0').toString(),   'M100 0');
+    assert.equal(svgpath('M +1e+2 0').toString(),  'M100 0');
+    assert.equal(svgpath('M 1e-2 0').toString(),   'M0.01 0');
+    assert.equal(svgpath('M 0.1e-2 0').toString(), 'M0.001 0');
+    assert.equal(svgpath('M .1e-2 0').toString(),  'M0.001 0');
+  });
+
+  it('repeated', function () {
+    assert.equal(svgpath('M 0 0 100 100').toString(),  'M0 0L100 100');
+    assert.equal(svgpath('m 0 0 100 100').toString(),  'M0 0l100 100');
+    assert.equal(svgpath('M 0 0 R 1 1 2 2').toString(),  'M0 0R1 1 2 2');
+    assert.equal(svgpath('M 0 0 r 1 1 2 2').toString(),  'M0 0r1 1 2 2');
+  });
+
+  it('errors', function () {
+    assert.equal(svgpath('0').err, 'SvgPath: bad command 0 (at pos 0)');
+    assert.equal(svgpath('U').err, 'SvgPath: bad command U (at pos 0)');
+    assert.equal(svgpath('z').err, 'SvgPath: string should start with `M` or `m`');
+    assert.equal(svgpath('M+').err, 'SvgPath: param should start with 0..9 or `.` (at pos 2)');
+    assert.equal(svgpath('M00').err, 'SvgPath: numbers started with `0` such as `09` are ilegal (at pos 1)');
+    assert.equal(svgpath('M0e').err, 'SvgPath: invalid float exponent (at pos 3)');
   });
 });
--- test/transform.js
+++ test/transform.js
@@ -2,32 +2,38 @@
 
 'use strict';
 
-var assert  = require('assert');
 
-var SvgPath = require('../');
+var assert  = require('assert');
+var svgpath = require('../');
+
 
 describe('Transform', function () {
 
   describe('translate', function () {
     it('x only', function () {
-      assert.equal('M30 10L35 15', new SvgPath('M10 10 L15 15').transform('translate(20)').toString());
+      assert.equal(
+        svgpath('M10 10 L15 15').transform('translate(20)').toString(),
+        'M30 10L35 15'
+      );
     });
 
     it('x and y', function () {
-      assert.equal('M30 20L35 25', new SvgPath('M10 10 L15 15').transform('translate(20,10)').toString());
+      assert.equal(
+        svgpath('M10 10 L15 15').transform('translate(20,10)').toString(),
+        'M30 20L35 25');
     });
 
     it('x and y with relatives curves', function () {
       assert.equal(
-        'M30 20c15 15 20 10 15 15',
-        new SvgPath('M10 10 c15 15, 20 10, 15 15').transform('translate(20,10)').toString()
+        svgpath('M10 10 c15 15, 20 10, 15 15').transform('translate(20,10)').toString(),
+        'M30 20c15 15 20 10 15 15'
       );
     });
 
     it('x and y with absolute curves', function () {
       assert.equal(
-        'M30 20C35 25 40 20 35 25',
-        new SvgPath('M10 10 C15 15, 20 10, 15 15').transform('translate(20,10)').toString()
+        svgpath('M10 10 C15 15, 20 10, 15 15').transform('translate(20,10)').toString(),
+        'M30 20C35 25 40 20 35 25'
       );
     });
   });
@@ -35,96 +41,127 @@ describe('Transform', function () {
 
   describe('rotate', function () {
     it('rotate by 90 degrees about point(10, 10)', function () {
-      assert.equal('M10 10L10 15', new SvgPath('M10 10L15 10').transform('rotate(90, 10, 10)').round(0).toString());
+      assert.equal(
+        svgpath('M10 10L15 10').transform('rotate(90, 10, 10)').round(0).toString(),
+        'M10 10L10 15'
+      );
     });
 
     it('rotate by -90 degrees about point (0,0)', function () {
-      assert.equal('M10 0L20 0', new SvgPath('M0 10L0 20').transform('rotate(-90)').round(0).toString());
+      assert.equal(
+        svgpath('M0 10L0 20').transform('rotate(-90)').round(0).toString(),
+        'M10 0L20 0'
+      );
     });
   });
 
 
   describe('scale', function () {
     it('scale picture by 2', function () {
-      assert.equal('M10 10L30 40', new SvgPath('M5 5L15 20').transform('scale(2)').toString());
+      assert.equal(
+        svgpath('M5 5L15 20').transform('scale(2)').toString(),
+        'M10 10L30 40'
+      );
     });
 
     it('scale picture with x*0.5 and y*1.5', function () {
-      assert.equal('M2.5 7.5L15 30', new SvgPath('M5 5L30 20').transform('scale(.5, 1.5)').toString());
+      assert.equal(
+        svgpath('M5 5L30 20').transform('scale(.5, 1.5)').toString(),
+        'M2.5 7.5L15 30'
+      );
     });
 
     it('scale picture with x*0.5 and y*1.5 with relative elements', function () {
       assert.equal(
-        'M2.5 7.5c7.5 22.5 10 15 7.5 22.5',
-        new SvgPath('M5 5c15 15, 20 10, 15 15').transform('scale(.5, 1.5)').toString()
+        svgpath('M5 5c15 15, 20 10, 15 15').transform('scale(.5, 1.5)').toString(),
+        'M2.5 7.5c7.5 22.5 10 15 7.5 22.5'
       );
     });
   });
+
 
   describe('skew', function () {
     // SkewX matrix [ 1, 0, 4, 1, 0, 0 ],
     // x = x*1 + y*4 + 0 = x + y*4
     // y = x*0 + y*1 + 0 = y
     it('skewX', function () {
-      assert.equal('M25 5L95 20', new SvgPath('M5 5L15 20').transform('skewX(75.96)').round(0).toString());
+      assert.equal(
+        svgpath('M5 5L15 20').transform('skewX(75.96)').round(0).toString(),
+        'M25 5L95 20'
+      );
     });
 
     // SkewY matrix [ 1, 4, 0, 1, 0, 0 ],
     // x = x*1 + y*0 + 0 = x
     // y = x*4 + y*1 + 0 = y + x*4
     it('skewY', function () {
-      assert.equal('M5 25L15 80', new SvgPath('M5 5L15 20').transform('skewY(75.96)').round(0).toString());
+      assert.equal(
+        svgpath('M5 5L15 20').transform('skewY(75.96)').round(0).toString(),
+        'M5 25L15 80'
+      );
     });
   });
+
 
   describe('matrix', function () {
     // x = x*1.5 + y/2 + ( absolute ? 10 : 0)
     // y = x/2 + y*1.5 + ( absolute ? 15 : 0)
     it('path with absolute segments', function () {
       assert.equal(
-        'M20 25C55 70 32.5 42.5 62.5 52.5',
-        new SvgPath('M5 5 C20 30 10 15 30 15').transform('matrix(1.5, 0.5, 0.5, 1.5 10, 15)').toString()
+        svgpath('M5 5 C20 30 10 15 30 15').transform('matrix(1.5, 0.5, 0.5, 1.5 10, 15)').toString(),
+        'M20 25C55 70 32.5 42.5 62.5 52.5'
       );
     });
 
     it('path with relative segments', function () {
       assert.equal(
-        'M20 25c21 23 22.5 27.5 45 55',
-        new SvgPath('M5 5 c10 12 10 15 20 30').transform('matrix(1.5, 0.5, 0.5, 1.5 10, 15)').toString()
+        svgpath('M5 5 c10 12 10 15 20 30').transform('matrix(1.5, 0.5, 0.5, 1.5 10, 15)').toString(),
+        'M20 25c21 23 22.5 27.5 45 55'
       );
     });
   });
 
-  describe('segment replacement', function () {
-    it('[H,V] => L', function () {
-      assert.equal(
-        'M100 100L110 100 110 110ZM200 200L215 200 215 190',
-        new SvgPath('M0 0 H 10 V 10 Z M 100 100 h 15 v -10').transform('translate(100,100)').toString()
-      );
-    });
-  });
 
-  describe('multiple transforms', function () {
+  describe('combinations', function () {
     it('scale + translate', function () {
       assert.equal(
-        'M100 100L120 130 140 130',
-        new SvgPath('M0 0 L 10 10 20 10').transform('translate(100,100) scale(2,3)').toString()
+        svgpath('M0 0 L 10 10 20 10').transform('translate(100,100) scale(2,3)').toString(),
+        'M100 100L120 130 140 130'
       );
     });
 
     it('scale + rotate', function () {
       assert.equal(
-        'M0 0L-30 20-30 40',
-        new SvgPath('M0 0 L 10 10 20 10').transform('rotate(90) scale(2,3)').round(0).toString()
+        svgpath('M0 0 L 10 10 20 10').transform('rotate(90) scale(2,3)').round(0).toString(),
+        'M0 0L-30 20-30 40'
       );
     });
 
     it('rotate + skewX', function () {
       assert.equal(
-        'M0 0L140 30 160 30',
-        new SvgPath('M0 0 L 10 10 20 10').transform('skewX(75.96) scale(2,3)').round(0).toString()
+        svgpath('M0 0 L 10 10 20 10').transform('skewX(75.96) scale(2,3)').round(0).toString(),
+        'M0 0L140 30 160 30'
+      );
+    });
+
+    it('empty transforms', function () {
+      assert.equal(
+        svgpath('M0 0 L 10 10 20 10')
+          .transform('rotate(0) scale(1,1) translate(0,0) skewX(0) skewY(0)')
+          .round(0)
+          .toString(),
+        'M0 0L10 10 20 10'
       );
     });
   });
 
+
+  describe('misc', function () {
+    it('segment replacement [H,V] => L', function () {
+      assert.equal(
+        svgpath('M0 0 H 10 V 10 Z M 100 100 h 15 v -10').transform('translate(100,100)').toString(),
+        'M100 100L110 100 110 110ZM200 200L215 200 215 190'
+      );
+    });
+  });
 });
