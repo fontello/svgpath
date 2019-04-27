0b0488a549950afaee37c3d9c01853d07d034f54
Luc Vauvillier <luc.vauvillier@gmail.com>
2019-04-27 18:28:15 +0200

    Fix signatures in typescript definition (#32)
    * Fix string typo in typescript definition
    * Fix matrix signature in typescript definition

--- index.d.ts
+++ index.d.ts
@@ -8,11 +8,11 @@ declare module "svgpath" {
     rotate(angle: number, rx?: number, ry?: number): SvgPath;
     skewX(degrees: number): SvgPath;
     skewY(degrees: number): SvgPath;
-    matrix(m1: number, m2: number, m3: number, m4: number, m5: number, m6: number): SvgPath;
+    matrix(m: number[]): SvgPath;
     transform(str: string): SvgPath;
     unshort(): SvgPath;
     unarc(): SvgPath;
-    toString(): String;
+    toString(): string;
     round(precision: number): SvgPath;
     iterate(iterator: (segment: any[], index: number, x: number, y: number) => void, keepLazyStack?: boolean): SvgPath;
   }
